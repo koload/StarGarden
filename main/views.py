30af877934aa033b6864e0b0495b670a5c333854
koload <koload20@gmail.com>
2024-03-29 19:12:03 +0100

    Basic configurations in main/urls, main/views, stargarden/settings and stargarden/urls

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+
 def index(response):
     return HttpResponse("testview")
+
+def v1(response):
+    return HttpResponse("view1 working? sadasworking.")
 # Create your views here.
-
-
